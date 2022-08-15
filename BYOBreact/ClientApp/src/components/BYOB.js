3180d85d7c89f009d269601a741bd7de6be345e6
mannal26112000 <mannal26112000@gmail.com>
2022-08-15 18:40:03 +0500

    rendering data

--- BYOBreact/ClientApp/src/components/BYOB.js
+++ BYOBreact/ClientApp/src/components/BYOB.js
@@ -10,7 +10,7 @@ export class BYOB extends Component {
         super(props);
         //this.state = { currentCount: 0 };
         //this.incrementCounter = this.incrementCounter.bind(this);
-        this.state = { viewModel: {meats:[],vegetables:[]}, name: "manal" }
+        this.state = { viewModel: { meat: [], vegetables: [], sauces: [], burgerMuffins: [], Burger:[] } }
     }
     componentDidMount() {
         this.populateBurgerData();
@@ -24,81 +24,81 @@ export class BYOB extends Component {
         console.log(this.state.viewModel);
     }
 
-    render() {
+    //render() {
+    //    return (
+    //        <><button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+    //            <Form>
+    //                {/*<p>{this.state.viewModel.burgerMuffins[1].name}</p>*/}
+    //                {/*<p>{this.state.name}</p>*/}
+    //                <Form.Group className="mb-3" controlId="formBasicEmail">
+    //                    <Form.Label>Burger Name</Form.Label>
+    //                    <Form.Control type="text" placeholder="Enter your Burger Name" />
+    //                </Form.Group>
+
+    //                <Form.Select aria-label="Default select example">
+    //                    {/*{this.state.meat.map(m =>*/}
+    //                    {/*    <tr key={m.id}>*/}
+    //                    {/*        <option>{m.name}</option>*/}
+    //                    {/*    </tr>*/}
+    //                    {/*)}*/}
+
+    //                </Form.Select>
+
+    //                <Form.Select aria-label="Default select example">
+    //                    <option>Open this select menu</option>
+    //                    <option value="1">one</option>
+    //                    <option value="2">Two</option>
+    //                    <option value="3">Three</option>
+    //                </Form.Select>
+
+    //                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    //                    <Form.Check type="checkbox" label="With Cheese" />
+    //                </Form.Group>
+
+    //                <Button variant="primary" type="submit">
+    //                    Submit
+    //                </Button>
+
+    //            </Form></>
+    //    );
+    //}
+
+    static renderForecastsTable(meat) {
         return (
-            <><button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-                <Form>
-                    {/*<p>{this.state.viewModel.burgerMuffins[1].name}</p>*/}
-                    {/*<p>{this.state.name}</p>*/}
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Burger Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter your Burger Name" />
-                    </Form.Group>
-
-                    <Form.Select aria-label="Default select example">
-                        {/*{this.state.viewModel.meats.map(m =>*/}
-                        {/*    <tr key={m.id}>*/}
-                        {/*        <option>{m.name}</option>*/}
-                        {/*    </tr>*/}
-                        {/*)}*/}
-
-                    </Form.Select>
-
-                    <Form.Select aria-label="Default select example">
-                        <option>Open this select menu</option>
-                        <option value="1">one</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </Form.Select>
-
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="With Cheese" />
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-
-                </Form></>
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {meat.map(m =>
+                        <tr key={m.id}>
+                            <td>{m.name}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         );
     }
 
-    //static renderForecastsTable(meat) {
-    //    return (
-    //        <table className='table table-striped' aria-labelledby="tabelLabel">
-    //            <thead>
-    //                <tr>
-    //                    <th>Name</th>
-    //                </tr>
-    //            </thead>
-    //            <tbody>
-    //                {meat.map(m =>
-    //                    <tr key={m.id}>
-    //                        <td>{m.name}</td>
-    //                    </tr>
-    //                )}
-    //            </tbody>
-    //        </table>
-    //    );
-    //}
+    render() {
 
-    //render() {
-     
-    //    let contents = BYOB.renderForecastsTable(this.state.viewModel);
+        let contents = BYOB.renderForecastsTable(this.state.viewModel.meat);
 
-    //    return (
-    //        <div>
-    //            <h1 id="tabelLabel" >Weather forecast</h1>
-    //            <p>This component demonstrates fetching data from the server.</p>
-    //            {contents}
-    //        </div>
-    //    );
-    //}
+        return (
+            <div>
+                <h1 id="tabelLabel" >Weather forecast</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
 
     async populateBurgerData() {
         const response = await fetch('api/Burger/GetBurgerItems');
         const data = await response.json();
-        this.setState({ viewModel: data ,name:"burger"});
+        this.setState({ viewModel: data, meat: data.meats, vegetables: data.vegetables, sauces: data.sauces, burgerMuffins: data.burgerMuffins});
         console.log("data view:", this.state.viewModel.meats[0].name)
         console.log("data:", data);
     }
